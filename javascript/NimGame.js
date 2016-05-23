3243d22e6b6c1482496cfd2605ac279b2a907259
dieface <fockerlee@gmail.com>
2016-05-23 22:22:29 +0800

    Add 'Nim Game'

--- javascript/NimGame.js
+++ javascript/NimGame.js
@@ -3,5 +3,11 @@
  * @return {boolean}
  */
 var canWinNim = function(n) {
-
+  return n % 4 > 0;
 };
+
+var input = "4";
+var output = canWinNim(input);
+
+console.log('input: ', input);
+console.log('output: ', output);
